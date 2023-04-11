b2d0712be137801e8db858aed55ef5c53f3658f0
Your Name <you@example.com>
2023-04-11 11:57:16 -0300

    Projeto Filmes concluido

---
+++ Filmes/src/data/movies.js
@@ -0,0 +1,74 @@
+const DATA =
+[
+
+{
+    ID: '01',
+    NOME: 'Adão Negro',
+    nota: '8.8',
+    imagem: '1.jpg'
+},
+
+{
+    ID: '02',
+    NOME: 'Gato de Botas 2',
+    nota: '9.7',
+    imagem: '2.jpg'
+},
+
+{
+    ID: '03',
+    NOME: 'Avatar 2',
+    nota: '6.5',
+    imagem: '3.jpg'
+},
+
+{
+    ID: '04',
+    NOME: 'Pantera Negra',
+    nota: '7.4',
+    imagem: '4.jpg'
+},
+
+{
+    ID: '05',
+    NOME: 'Senhor Dos Aneis As Duas Torres',
+    nota: '9.6',
+    imagem: '5.jpg'
+},
+
+{
+    ID: '06',
+    NOME: 'O Hobbit Uma Jornada Inesperada',
+    nota: '9.9',
+    imagem: '6.jpg'
+},
+
+{
+    ID: '07',
+    NOME: 'Missão impossivel',
+    nota: '8.2',
+    imagem: '7.jpg'
+},
+
+{
+    ID: '08',
+    NOME: 'Vingadores Ultimato',
+    nota: '9.2',
+    imagem: '8.jpg'
+},
+
+{
+    ID: '09',
+    NOME: 'Truque De Mestre',
+    nota: '7.7',
+    imagem: '9.jpg'
+},
+
+{
+    ID: '10',
+    NOME: 'Doutor Estranho Multiverso da Loucura',
+    nota: '7.3',
+    imagem: '10.jpg'
+},
+
+]
